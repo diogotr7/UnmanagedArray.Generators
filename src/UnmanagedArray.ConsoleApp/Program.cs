940ca594595cd4d588e63f593148344ad52b66e5
Diogo Trindade <diogotr7@gmail.com>
2023-09-05 14:54:13 +0100

    initial commit

---
+++ src/UnmanagedArray.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using UnmanagedArrayGenerator;
+
+namespace UnmanagedArray.ConsoleApp;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var sizeOf = Unsafe.SizeOf<TestStruct>();
+        var expectedSize = 10 * Unsafe.SizeOf<int>();
+        if (sizeOf != expectedSize)
+            throw new Exception($"Expected size of {expectedSize}, got {sizeOf}");
+    }
+}
+
+[UnmanagedArray(typeof(int), 10)]
+public readonly partial record struct TestStruct;
---
+++ src/UnmanagedArray.Generators/StructInfo.cs
@@ -0,0 +1,11 @@
+namespace UnmanagedArray.Generators;
+
+public class StructInfo
+{
+    public string Namespace { get; set; }
+    public string ParentStruct { get; set; }
+    public string ChildStruct { get; set; }
+    public int Count { get; set; }
+    public bool IsRecordStruct { get; set; }
+    public bool IsReadOnly { get; set; }
+}
